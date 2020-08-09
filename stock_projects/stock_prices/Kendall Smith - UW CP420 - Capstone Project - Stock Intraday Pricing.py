fb329e968ba31e6d158c42d1ef39392b3c6a11b5
KennSmithDS <smith.kendall.lee@gmail.com>
2020-08-09 10:56:06 -0700

    Update Kendall Smith - UW CP420 - Capstone Project - Stock Intraday Pricing.py

--- stock_projects/stock_prices/Kendall Smith - UW CP420 - Capstone Project - Stock Intraday Pricing.py
+++ stock_projects/stock_prices/Kendall Smith - UW CP420 - Capstone Project - Stock Intraday Pricing.py
@@ -78,7 +78,7 @@ def scaled_histogram(df):
     plt.show()
 
 def get_equity_data(ticker, end_date, start_date):
-    api_key = 'SknGuPiaoAlgo%40AMER.OAUTHAP'
+    api_key = f'{api_key_here}%40AMER.OAUTHAP'
     end_date = '1526108400000'
     start_date = '1451606400000'
     #start_date = '1451635200000'
